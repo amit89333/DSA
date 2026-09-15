044f49c1fa0f237d620be8b9141a08192ca7da1d
AMIT YADAV <amit.yadav.cs28@iilm.edu>
2026-09-15 09:44:22 +0530

    Solved: Implement Stack using Queues [Easy]

---
+++ LeetCode/Easy/Implement Stack using Queues/solution.java
@@ -0,0 +1,25 @@
+class MyStack {
+
+    Queue<Integer> q = new LinkedList<>();
+
+    public void push(int x) {
+        q.add(x);
+        int n = q.size();
+
+        for (int i = 0; i < n - 1; i++) {
+            q.add(q.remove());
+        }
+    }
+
+    public int pop() {
+        return q.remove();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
