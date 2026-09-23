0d431d61b2aaf255232298a7531760027e8d2fe4
AMIT YADAV <amit.yadav.cs28@iilm.edu>
2026-09-23 09:43:36 +0530

    Solved: Lucky Numbers in a Matrix [Easy]

--- Arrays & Hashing/Easy/Lucky Numbers in a Matrix/solution.java
+++ Arrays & Hashing/Easy/Lucky Numbers in a Matrix/solution.java
@@ -1,34 +1,26 @@
 class Solution {
     public List<Integer> luckyNumbers(int[][] matrix) {
-        int N = matrix.length, M = matrix[0].length;
-
-        List<Integer> rowMin = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            int rMin = Integer.MAX_VALUE;
-            for (int j = 0; j < M; j++) {
-                rMin = Math.min(rMin, matrix[i][j]);
-            }
-            rowMin.add(rMin);
-        }
-
-        List<Integer> colMax = new ArrayList<>();
-        for (int i = 0; i < M; i++) {
-            int cMax = Integer.MIN_VALUE;
-            for (int j = 0; j < N; j++) {
-                cMax = Math.max(cMax, matrix[j][i]);
-            }
-            colMax.add(cMax);
-        }
-
-        List<Integer> luckyNumbers = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (matrix[i][j] == rowMin.get(i) && matrix[i][j] == colMax.get(j)) {
-                    luckyNumbers.add(matrix[i][j]);
-                }
+    List<Integer> ans = new ArrayList<>();
+    for (int i=0; i<matrix.length; i++){
+        int min =matrix[i][0];
+        int col =0;
+        for (int j=1; j<matrix[i].length; j++){
+            if (matrix[i][j]<min){
+                min = matrix[i][j];
+                col = j;
             }
         }
-
-        return luckyNumbers;
+        boolean lucky = true;
+        for (int k=0; k<matrix.length; k++){
+            if (matrix[k][col]> min){
+                lucky = false;
+                break;
+            }
+        }
+        if(lucky){
+            ans.add(min);
+        }
+    }
+    return ans;
     }
 }
